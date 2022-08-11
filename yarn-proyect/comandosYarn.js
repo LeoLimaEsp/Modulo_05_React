87e9a95c44e12f2a0ee4813a343c1ce882fed2bc
leonel <leonel.limae@hotmail.com>
2022-08-10 20:03:06 -0500

    yarn

---
+++ yarn-proyect/comandosYarn.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * Instalar paquetes
+ * 
+ * download dependecies - yarn
+ * 
+ * install yarn add package_name
+ * 
+ * install dev dependecies - yarn add --dev package_name
+ * 
+ * uninstall package - yarn remove 
+ * 
+ * update  - yarn upgrade actualiza las dependecias del package.json
+ */
