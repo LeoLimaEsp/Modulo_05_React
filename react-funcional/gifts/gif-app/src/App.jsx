4eed73d9f9444646e6d3dbb90e31aba9f10591bb
leonel <leonel.limae@hotmail.com>
2022-08-17 21:45:58 -0500

    Gift terminado

--- react-funcional/gifts/gif-app/src/App.jsx
+++ react-funcional/gifts/gif-app/src/App.jsx
@@ -1,20 +1,24 @@
-
 import { useState } from 'react'
 import Grid from './components/Grid'
 import AddCategory from './components/AddCategory'
-import './index.css'
+
 function App() {
-  const [categories, setCategories] = useState(['Nascar', 'Dragon Ball', 'One Piece'])
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball', 'One Piece'])
+
+  const onAddCategory = (newCategory) => {
+    if(categories.includes(newCategory)) return
+    setCategories([newCategory,...categories])
+  }
+
   return (
     <>
       <h1>Gif App</h1>
-      <div className='container'>
+      <AddCategory onAddCategory={onAddCategory}/>
       {
-        categories.map((category) => (<Grid category={category}/>))
+        categories.map((category) => (<Grid key={category} category={category}/>))
       }
-      </div>
       
-      <AddCategory/>
+     
     </>
   )
 }
